4f56dc849d592accad61977b9f8950f3bba80237
Aranxa Codina <tassa780@gmail.com>
2023-03-14 23:15:59 +0100

    CM-133 : azure function works

--- deposit_extract/DepositExtract.cs
+++ deposit_extract/DepositExtract.cs
@@ -10,20 +10,21 @@ namespace deposit_extract
     public class DepositExtract
     {
         [FunctionName("DepositExtract")]
-        public void Run ([TimerTrigger("0 * * * * *")] TimerInfo myTimer, ILogger log)
+        public void Run ([TimerTrigger("0 0 2 * * *")] TimerInfo myTimer, ILogger log)
         {
-            //string apiKey = Environment.GetEnvironmentVariable("API_KEY");
-            //string apiUrl = Environment.GetEnvironmentVariable("API_URL");
+            string apiKey = Environment.GetEnvironmentVariable("API_KEY");
+            string apiUrl = Environment.GetEnvironmentVariable("API_URL");
 
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
             using (HttpClient client = new HttpClient())
             {
-                // Ajouter l'en-tête API-KEY à la demande
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("API-KEY", "0d8eedb6-b1a1-467f-bf4a-7057174f1cd9");
-
                 // Faire la requête HTTP GET et récupérer la réponse
-                client.GetAsync("https://api.test.deposit.ovh/v1/jobs?job=extract:users");
+                var url = $"{apiUrl}{apiKey}";
+                var response = client.GetAsync($"{apiUrl}{apiKey}");
+                var responseString = response.Result.Content.ToString();
+                log.Log(LogLevel.Debug, url);
+                log.Log(LogLevel.Debug, responseString);
             }
         }
     }
